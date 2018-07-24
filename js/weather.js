f0c6e4cef1e393a8f2b9c1375673f0e25c6aa186
ANDRE-Ani <patr_and@yahoo.fr>
2018-07-24 15:29:55 +0200

    JS test

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,48 @@
 // weather with geoloc from openweathermap
 
-var weatherL = {
+
+openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather'
+if (window.navigator && window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(function(position) {
+        $.getJSON(openWeatherMap, {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+            units: 'metric',
+            lang: 'fr',
+            APPID: '8af0f894920fd7fcf2e0dc3b48605453'
+        }).done(function(weather) {
+            // console.log(weather)
+            town = weather.name;
+            tempL = weather.main.temp;
+            humL = weather.main.humidity;
+            pressL = weather.main.pressure;
+            speedL = weather.wind.speed;
+            desc = weather.weather[0].description;
+            document.getElementById("weather").innerHTML = "Ville : " + town + "<br>" + " Conditions actuelles : " + desc + "<br>" + "Température : " + tempL + "°" + " Humidité : " + humL + "%" + "<br>" + "Pression : " + pressL + "°" + " Vent : " + speedL;
+
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* var weatherL = {
     openWeatherMap: "",
     town: "",
     tempL: "",
     humL: "",
     pressL: "",
     speedL: "",
-    desc: "",
+    desc: "", 
 
     weatherLoc: function() {
 
@@ -34,4 +69,4 @@ var weatherL = {
             })
         }
     }
-}
+} */
