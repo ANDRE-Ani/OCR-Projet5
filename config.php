57ce584ac5a58fd1cbe48dcd6d3f08fe4b26eb3e
ANDRE_Ani <ANDRE-Ani@users.noreply.github.com>
2019-01-09 07:51:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 // Configuration des principales variables, indentifiants pour la BDD
 
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'boutique_bdd');
-    define('DB_PASS', 'cybergoth1978');
-    define('DB_NAME', 'boutique_p5ocr');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
     
     define('SITE_NAME', '5Project');
     define('AUTEUR', 'Patrice Andreani');
-    define('UPLOAD_DIR', '/home/boutique/public_html/dev/p5ocr/uploads/');
+    define('UPLOAD_DIR', '/');
